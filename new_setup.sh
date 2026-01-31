ce6100b9c5a87d2fb8a0f459e93feda85562f7bd
Jose Vega <josevega234@gmail.com>
2026-01-31 12:12:05 +0100

    new setup

--- new_setup.sh
+++ new_setup.sh
@@ -13,12 +13,6 @@ error_exit() {
 }
 trap 'error_exit $LINENO "$BASH_COMMAND"' ERR
 
-# COMPROBAR QUE ES ARCH LINUX
-# if ! grep -qi "arch" /etc/os-release; then
-#   echo "Este script solo funciona en Arch Linux."
-#   exit 1
-# fi
-
 # COMPROBAR DEPENDENCIAS
 for dep in gum git curl; do
   if ! command -v "$dep" &>/dev/null; then
@@ -46,295 +40,267 @@ install_packages() {
   done
 }
 
+function install {
+  if ! pacman -Qi "$1" &>/dev/null; then
+    echo "Installing $1..."
+    paru -S "$1" --noconfirm
+  else
+    echo "$1 is already installed. Skipping..."
+  fi
+}
+
+install_docker() {
+  paru -S --needed --noconfirm docker docker-compose docker-buildx
+
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo usermod -aG docker "$USER"
+  sudo groupadd dialout
+  sudo usermod -aG dialout "$USER"
+
+  #docker-clean
+  curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean |
+    sudo tee /usr/local/bin/docker-clean >/dev/null &&
+    sudo chmod +x /usr/local/bin/docker-clean
+}
+
 # ARRAYS DE PAQUETES (revisar duplicados antes)
 packages_common_utils=(
-  "acpi"
-  "adw-gtk-theme"
-  "alsa-utils"
-  "archlinux-xdg-menu"
-  "ark"
-  "bat"
-  "bat-extras"
-  "bibata-cursor-theme"
-  "bind"
-  "blueman"
-  "bluez"
-  "bluez-utils"
-  "brightnessctl"
-  "btop"
-  "cava"
-  "cmake"
-  "cpio"
-  "curl"
-  "dkms"
-  "docker"
-  "docker-compose"
-  "downgrade"
-  "eww-git"
-  "eza"
-  "fastfetch"
-  "fzf"
-  "git"
-  "git-lfs"
-  "github-cli"
-  "glibc"
-  "gnome-keyring"
-  "go"
-  "gtk4"
-  "gvfs"
-  "gvfs-mtp"
-  "gvfs-smb"
-  "kitty"
-  "lazygit"
-  "less"
-  "lib32-pipewire"
-  "libva-nvidia-driver"
-  "lsd"
-  "luarocks"
-  "ly"
-  "man-db"
-  "man-pages"
-  "matugen-bin"
-  "meson"
-  "mise"
-  "mlocate"
-  "ncdu"
-  "net-tools"
-  "network-manager-applet"
-  "networkmanager-openvpn"
-  "ntfs-3g"
-  "nwg-look"
-  "pacman-contrib"
-  "pavucontrol"
-  "pipewire"
-  "pipewire-alsa"
-  "pipewire-audio"
-  "pipewire-pulse"
-  "pkgconf-pkg-config"
-  "pkgfile"
-  "playerctl"
-  "python-pywalfox"
-  "python-gobject"
-  "python-pip"
-  "python-pipx"
-  "python-pynvim"
-  "qt5ct-kde"
-  "qt6ct-kde"
-  "reflector"
-  "ripgrep"
-  "rsync"
-  "sad"
-  "sshfs"
-  "superfile"
-  "starship"
-  "stow"
-  "tealdeer"
-  "tela-circle-icon-theme-dracula"
-  "tmux"
-  "unarchiver"
-  "unzip"
-  "uv"
-  "wallust"
-  "wget"
-  "wireguard-tools"
-  "wireplumber"
-  "yt-dlp"
-  "zoxide"
-  "zsh"
-  "zstd"
-)
-
-packages_common_x11=(
-  "xorg"
-  "xsel"
-  "dex"
-  "xdotool"
-  "xclip"
-  "cliphist"
-  "xinput"
-  "rofi"
-  "polybar"
-  "dunst"
-  "feh"
-  "maim"
-  "picom"
+  7zip
+  acpi
+  adw-gtk-theme
+  alsa-utils
+  archlinux-xdg-menu
+  ark
+  bat
+  bat-extras
+  bibata-cursor-theme
+  bind
+  blueman
+  bluez
+  bluez-utils
+  brightnessctl
+  btop
+  cava
+  cmake
+  cpio
+  cpupower
+  curl
+  dkms
+  downgrade
+  eww
+  eza
+  fastfetch
+  foot
+  fzf
+  git
+  git-lfs
+  git-delta
+  github-cli
+  glibc
+  gnome-keyring
+  go
+  gtk4
+  gvfs
+  gvfs-mtp
+  gvfs-smb
+  gwenview
+  kitty
+  lazygit
+  less
+  lib32-pipewire
+  libva-nvidia-driver
+  luarocks
+  man-db
+  man-pages
+  matugen
+  meson
+  mise
+  mlocate
+  ncdu
+  neofetch
+  net-tools
+  networkmanager
+  network-manager-applet
+  networkmanager-openvpn
+  ntfs-3g
+  nwg-look
+  openssh
+  os-prober
+  pacman-contrib
+  pavucontrol
+  pipewire
+  pipewire-alsa
+  pipewire-audio
+  pipewire-pulse
+  pkgconf-pkg-config
+  pkgfile
+  playerctl
+  python-gobject
+  python-pip
+  python-pipx
+  python-pynvim
+  python-pywalfox
+  python-kikit
+  qt5ct-kde
+  qt6ct-kde
+  reflector
+  ripgrep
+  rsync
+  sad
+  sshfs
+  starship
+  stow
+  superfile
+  tealdeer
+  tela-circle-icon-theme-dracula
+  tmux
+  unarchiver
+  unrar
+  unzip
+  uv
+  wallust
+  wget
+  wireguard-tools
+  wireplumber
+  yt-dlp
+  zip
+  zoxide
+  zsh
+  zstd
 )
 
 packages_common_wayland=(
-  "qt5-wayland"
-  "qt6-wayland"
-  "egl-wayland"
-  "wlr-randr"
-  "wlogout"
-  "wl-clipboard"
-  "copyq"
-  "rofi-wayland"
-  "waybar"
-  "mako"
-  "swww"
+  copyq
+  egl-wayland
+  gpu-screen-recorder
+  grimblast
+  mako
+  qt5-wayland
+  qt6-wayland
+  rofi-wayland
+  swww
+  waybar
+  wl-clipboard
+  wlogout
+  wlr-randr
 )
 
 packages_hyprland=(
-  "hyprland"
-  "hyprutils"
-  "hyprpicker"
-  "hyprpolkitagent"
-  "hyprshot"
-  "xdg-desktop-portal-hyprland"
-  "hyprlock"
-  "pyprland"
-  "hypridle"
-  "uwsm"
+  ags-hyprpanel-git
+  hyprland
+  hyprutils
+  hyprpicker
+  hyprpolkitagent
+  hyprshot
+  xdg-desktop-portal-hyprland
+  hyprlock
+  pyprland
+  hypridle
+  uwsm
 )
 
 packages_niri=(
-  "niri"
-  "xwayland-satellite"
-  "xdg-desktop-portal-gnome"
-)
-
-packages_awesome=(
-  "awesome"
-  "lain"
-  "polkit-gnome"
-)
-
-packages_i3=(
-  "i3-wm"
-  "i3lock"
-  "autotiling"
+  niri
+  xwayland-satellite
+  xdg-desktop-portal-gnome
 )
 
 packages_apps=(
-  "clock-rs-git"
-  "dolphin"
-  "filelight"
-  "firefox"
-  "foliate"
-  "ghostty"
-  "gnome-disk-utility"
-  "imagemagick"
-  "lazydocker"
-  "lazygit"
-  "mpc"
-  "mpd"
-  "mpv"
-  "nano"
-  "neovim"
-  "nomacs"
-  "okular"
-  "orca-slicer-unstable-bin"
-  "qalculate-gtk"
-  "qbittorrent"
-  "rmpc"
-  "shortwave"
-  "superfile"
-  "vim"
-  "vscodium-bin"
-  "vscodium-bin-marketplace"
-  "yazi"
+  1password
+  blueberry
+  filelight
+  firefox
+  foliate
+  ghostty
+  gnome-disk-utility
+  imagemagick
+  lazydocker
+  lazygit
+  localsend
+  moc-pulse
+  mpc
+  mpd
+  mpv
+  nano
+  neovim
+  nomacs
+  okular
+  orca-slicer-bin
+  platformio-core
+  platformio-core-udev
+  qalculate-gtk
+  qbittorrent
+  rmpc
+  rose-pine-cursor
+  rose-pine-hyprcursor
+  rustdesk-bin
+  telegram-desktop
+  shortwave
+  sublime-text-4
+  superfile
+  thunar
+  thunar-volman
+  thunar-vcs-plugin
+  thunar-archive-plugin
+  thunar-media-tags-plugin
+  vim
+  yazi
 )
 
 packages_fonts=(
-  "maplemono-ttf"
-  "noto-fonts"
-  "noto-fonts-emoji"
-  "apple-fonts"
-  "ttf-ms-fonts"
-  "otf-font-awesome"
+  noto-fonts
+  noto-fonts-emoji
+  ttf-fantasque-nerd
+  ttf-jetbrains-mono-nerd
+  ttf-opensans
 )
 
 packages_firmware=(
-  "aic94xx-firmware"
+  aic94xx-firmware
 )
 
 packages_nvidia=(
-  "nvidia-dkms"
-  "lib32-nvidia-utils"
-  "nvidia-utils"
-  "nvidia-settings"
+  nvidia-dkms
+  lib32-nvidia-utils
+  nvidia-utils
+  nvidia-settings
 )
 
-# OPCIONES INTERACTIVAS
-choice_backup_hook=$(gum choose "Yes" "No" --header "¿Configurar hook de backup de /boot?")
-choice_microcode=$(gum choose "Intel" "AMD" "None" --header "¿Instalar microcode del procesador?")
-choice_nvidia=$(gum choose "Yes" "No" --header "¿Instalar drivers Nvidia?")
-choice_wm=$(gum choose "hyprland" "niri" "awesome" "i3" --no-limit --header "Elige window managers a instalar.")
-choice_apps=$(gum choose "Yes" "No" --header "¿Instalar aplicaciones?")
-choice_misc=$(gum choose "Yes" "No" --header "¿Instalar Ollama?")
-choice_fonts=$(gum choose "Yes" "No" --header "¿Instalar fonts?")
-
-# FUNCIONES DE INSTALACIÓN
-setup_backup_hook() {
-  if [[ "$choice_backup_hook" == "Yes" ]]; then
-    sudo mkdir -p /etc/pacman.d/hooks
-    sudo tee /etc/pacman.d/hooks/50-bootbackup.hook >/dev/null <<EOF
-[Trigger]
-Operation = Upgrade
-Operation = Install
-Operation = Remove
-Type = Path
-Target = usr/lib/modules/*/vmlinuz
-[Action]
-Depends = rsync
-Description = Backing up /boot...
-When = PostTransaction
-Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
-EOF
-  fi
-}
-
 install_window_managers() {
-  for choice in "${choice_wm[@]}"; do
-    case "$choice" in
-    hyprland) install_packages "${packages_hyprland[@]}" "${packages_common_wayland[@]}" ;;
-    niri) install_packages "${packages_niri[@]}" "${packages_common_wayland[@]}" ;;
-    awesome) install_packages "${packages_awesome[@]}" "${packages_common_x11[@]}" ;;
-    i3) install_packages "${packages_i3[@]}" "${packages_common_x11[@]}" ;;
-    esac
-  done
-}
-
-install_misc() {
-  if [[ "$choice_misc" == "Yes" ]]; then
-    curl -fsSL https://ollama.com/install.sh | sh
-  fi
+  install_packages "${packages_hyprland[@]}" "${packages_common_wayland[@]}" "${packages_niri[@]}"
 }
 
 install_microcode() {
-  case "$choice_microcode" in
-  Intel) install_packages intel-ucode ;;
-  AMD) install_packages amd-ucode ;;
-  esac
+  install_packages intel-ucode
 }
 
 install_fonts() {
-  [[ "$choice_fonts" == "Yes" ]] && install_packages "${packages_fonts[@]}"
+  install_packages "${packages_fonts[@]}"
 }
 
 install_nvidia_drivers() {
-  [[ "$choice_nvidia" == "Yes" ]] && install_packages "${packages_nvidia[@]}"
+  install_packages "${packages_nvidia[@]}"
 }
 
 install_apps() {
-  [[ "$choice_apps" == "Yes" ]] && install_packages "${packages_apps[@]}"
+  install_packages "${packages_apps[@]}"
 }
 
 # CREAR CARPETAS DE USUARIO
-mkdir -p "$USER_HOME/Code" "$USER_HOME/.local/bin" "$USER_HOME/.local/share/backgrounds" "$USER_HOME/.local/share/icons"
+mkdir -p "$USER_HOME/Code" "$USER_HOME/.local/bin"
 sudo mkdir -p "$OPT_USER"
 sudo chown -R "$USER:$USER" "$OPT_USER"
 
 # INSTALACIONES PRINCIPALES
-setup_backup_hook
 install_packages "${packages_common_utils[@]}"
 install_window_managers
 install_packages "${packages_firmware[@]}"
-install_misc
 install_microcode
-# install_fonts
-# install_nvidia_drivers
+install_fonts
+install_nvidia_drivers
 install_apps
+install_docker
 
 stow . --adopt
 ln -s ./gitconfig ~/.gitconfig
@@ -348,8 +314,5 @@ sudo chsh -s /usr/bin/zsh root
 echo "Habilitando servicios..."
 systemctl --user enable pipewire
 sudo systemctl enable bluetooth
-sudo systemctl enable docker
-sudo usermod -aG docker "$USER"
-sudo systemctl enable ollama
 
 echo "Script completado exitosamente."
