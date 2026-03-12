17c73300fc2e30fe8560daf9107d502efc3d6f9d
Jose Vega <josevega234@gmail.com>
2026-03-12 06:25:28 +0100

    updates

--- .etc/restore.sh
+++ .etc/restore.sh
@@ -4,6 +4,6 @@ set -eu pipefail
 
 sudo pacman -Syu --needed --noconfirm - < common.pkgs.txt
 
-paru -S --needed --noconfirm - < apps.pgks.txt
+paru -S --needed --noconfirm - < apps.pkgs.txt
 
 exit 0
