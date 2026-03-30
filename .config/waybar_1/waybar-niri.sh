edf9b486f96f4a041054dd9948bd65b34deedc76
Jose Vega <josevega234@gmail.com>
2026-03-30 18:12:14 +0200

    updates

--- .config/waybar_1/waybar-niri.sh
+++ .config/waybar_1/waybar-niri.sh
@@ -7,4 +7,4 @@ killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
 # Launch main
-waybar -c ~/.config/waybar/config-niri.jsonc -s ~/.config/waybar/style.css &
+waybar -c ~/.config/waybar_1/config-niri.jsonc -s ~/.config/waybar_1/style.css &
